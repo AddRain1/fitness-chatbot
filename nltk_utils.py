a83aca62840c686b241766241d6184028e5ca994
AddRain1 <77909535+AddRain1@users.noreply.github.com>
2024-12-27 23:15:12 -0800

    create flask app

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,28 +1,44 @@
-# NLTK frameowrk for natural language processing
-
-import nltk
-# nltk.download('punkt_tab') # package with pretraiend tokenizer
-
 import numpy as np
-
-# import porterstemmer from nltk
+import nltk
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-# Tokenize the sentence, or splitting a string into a list of words
+
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
 
-# Stemming the words, or reducing a word to its root form
+
 def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
     return stemmer.stem(word.lower())
 
-# Bag of words, or a representation of text that describes the occurrence of words within a document
-def bag_of_words(tokenized_sentence, all_words):
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
-    bag = np.zeros(len(all_words), dtype=np.float32)
-    for idx, w, in enumerate(all_words):
-        if w in tokenized_sentence:
-            bag[idx] = 1.0
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
 
     return bag
